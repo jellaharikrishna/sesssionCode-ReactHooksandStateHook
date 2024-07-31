2b4cb069fa3a7751ddc5e158761c194e66b99313
jellaharikrishna <rk40665@gmail.com>
2024-07-31 06:15:59 +0000

    React Hooks and State Hook

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import GreetUser from './components/GreetUser'
+import {GlobalStyle} from './styledComponents'
+
+const App = () => (
+  <>
+    <GlobalStyle />
+    <GreetUser />
+  </>
+)
+
+export default App
